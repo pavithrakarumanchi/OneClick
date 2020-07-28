06c81e61bf059cb1a66c65793940fe6733c74af8
pavithrakarumanchi <51071573+pavithrakarumanchi@users.noreply.github.com>
2020-07-28 22:22:41 +0530

    First Commit

---
+++ src/user/DeleteUser.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {isAuthenticated} from '../auth';
+import {remove} from './apiUser';
+import {signout} from '../auth';
+import {Redirect} from 'react-router-dom';
+
+class DeleteUser extends Component {
+    
+    state = {
+       redirect: false
+    };
+
+	deleteAccount = () => {
+       const token = isAuthenticated().token;
+       const userId = this.props.userId;
+       remove(userId,token)
+       .then(data => {
+       	if(data.error) {
+       		console.log(data.error)
+       	}
+       	else
+       	{
+       		//Signout user and redirect
+            signout(() => console.log("User is deleted"));
+            this.setState({redirect: true});
+       	}
+       })
+	}
+	deleteConfirmed = () => {
+		let answer = window.confirm("Are you sure you want to delete your account?")
+		if(answer) {
+			this.deleteAccount()
+		}
+	}
+	render() {
+		if(this.state.redirect) {
+			return <Redirect t0="/" />
+		}
+		return (
+			 <button onClick={this.deleteConfirmed} className="btn btn-raised btn-danger">
+                                         Delete Profile
+                                  </button>
+			);
+	}
+}
+
+export default DeleteUser;
